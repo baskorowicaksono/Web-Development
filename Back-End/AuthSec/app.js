e034c9962b08f45983cc5b35fb0111e5abc9f077
baskorowicaksono <baskoroadi.wicaksono@gmail.com>
2021-06-24 14:11:57 +0700

    Learning Basic Authentication and Security. This Commit is Level 1 - Email and Password Validation.

---
+++ Back-End/AuthSec/app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+
+const express = require("express");
+// const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true});
+
+const userSchema = {
+    email : String,
+    password : String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+// const user = new User();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended : true}));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const userName = req.body.username;
+    const userPass = req.body.password;
+    
+    const newUser = new User({
+        email : userName,
+        password : userPass
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+})
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", function(req, res){
+    const userName = req.body.username;
+    const userPass = req.body.password;
+
+    User.findOne({email : userName}, function(err, result){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(result){
+                if(result.password === userPass){
+                    res.render("secrets");
+                }
+                else{
+                    res.send("Login Failed, Refresh and check your credentials");
+                }
+            }
+            else{
+                res.send("Account not found, please try again");
+            }
+        }
+    })
+})
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
+
